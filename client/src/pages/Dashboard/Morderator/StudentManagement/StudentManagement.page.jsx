20caa430eb5c247665f4eb39048a82726a0d0b31
Huy Đông <63062355+dong-huy@users.noreply.github.com>
2022-03-12 17:39:58 +0700

    update table
    update table

--- client/src/pages/Dashboard/Morderator/StudentManagement/StudentManagement.page.jsx
+++ client/src/pages/Dashboard/Morderator/StudentManagement/StudentManagement.page.jsx
@@ -1,79 +1,142 @@
-import React, { useState } from "react";
-import Body from '../../../../components/Body/Body.component';
-import Header from '../../../../components/Header/Header.component';
-import styles from './StudentManagement.module.scss'
+import React, { useEffect, useState } from "react";
+import Body from "../../../../components/Body/Body.component";
+import Header from "../../../../components/Header/Header.component";
+import styles from "./StudentManagement.module.scss";
 import FilterTable from "../../../../ui/Table/FilterTable/FilterTable.component";
 import SMTypeModel from "../../../../models/SMTypeModel";
 
 function StudentManagement() {
-    const studentsList = [
-        {
-            id: 1,
-            fullName: "Truong Dong Huy",
-            studentID: "24211206538",
-            email: "huydongtruong@gmail.com",
-            phoneNumber: "0359352931",
-            gpa: 3,
-            capstoneType: "Capstone 2",
-            codingLevel: "Level 1",
-            remainCredits: 0,
-            isInternship: true,
-        },
-        {
-            id: 1,
-            fullName: "Truong Dong Huy",
-            studentID: "24211206538",
-            email: "huydongtruong@gmail.com",
-            phoneNumber: "0359352931",
-            gpa: 3,
-            capstoneType: "Capstone 2",
-            codingLevel: "Level 1",
-            remainCredits: 0,
-            isInternship: true,
-        },
-        {
-            id: 1,
-            fullName: "Truong Dong Huy",
-            studentID: "24211206538",
-            email: "huydongtruong@gmail.com",
-            phoneNumber: "0359352931",
-            gpa: 3,
-            capstoneType: "Capstone 2",
-            codingLevel: "Level 1",
-            remainCredits: 0,
-            isInternship: true,
-        },
-        {
-            id: 1,
-            fullName: "Truong Dong Huy",
-            studentID: "24211206538",
-            email: "huydongtruong@gmail.com",
-            phoneNumber: "0359352931",
-            gpa: 3,
-            capstoneType: "Capstone 2",
-            codingLevel: "Level 1",
-            remainCredits: 0,
-            isInternship: true,
-        },
-    ];
-    return (
-        <div className = {styles['student-management']}>
-            <Header>
-                <div>
-                    <h5>Student Management</h5>
-                </div>
-            </Header>
-            <Body>
-                <div className = {styles['filter-table']}>
-                    <FilterTable
-                        columns = {SMTypeModel}
-                        rows = {studentsList}
-                    />
-                </div>                
-            </Body>
-            
+  const [students, setStudents] = useState([
+    {
+      id: 1,
+      fullName: "Truong Dong Huy",
+      studentID: "24211206538",
+      email: "huydongtruong@gmail.com",
+      phoneNumber: "0359352931",
+      gpa: 3,
+      capstoneType: "Capstone 2",
+      codingLevel: "Level 1",
+      remainCredits: 0,
+      isInternship: true,
+    },
+    {
+      id: 2,
+      fullName: "Truong Dong Dat",
+      studentID: "24211206538",
+      email: "huydongtruong@gmail.com",
+      phoneNumber: "0359352931",
+      gpa: 3,
+      capstoneType: "Capstone 2",
+      codingLevel: "Level 1",
+      remainCredits: 0,
+      isInternship: true,
+    },
+    // {
+    //     id: 3,
+    //     fullName: "AAAAAA",
+    //     studentID: "24211206538",
+    //     email: "huydongtruong@gmail.com",
+    //     phoneNumber: "0359352931",
+    //     gpa: 3,
+    //     capstoneType: "Capstone 2",
+    //     codingLevel: "Level 1",
+    //     remainCredits: 0,
+    //     isInternship: true,
+    // },
+    // {
+    //     id: 4,
+    //     fullName: "CCC",
+    //     studentID: "24211206538",
+    //     email: "huydongtruong@gmail.com",
+    //     phoneNumber: "0359352931",
+    //     gpa: 3,
+    //     capstoneType: "Capstone 2",
+    //     codingLevel: "Level 1",
+    //     remainCredits: 0,
+    //     isInternship: true,
+    // },
+  ]);
+
+  const [currentRow, setCurrentRow] = useState({});
+
+  const add = () => {
+    let newStudents = [
+      {
+        id: "Untitled",
+        fullName: "",
+        studentID: "",
+        email: "",
+        phoneNumber: "",
+        gpa: 0,
+        capstoneType: "",
+        codingLevel: "",
+        remainCredits: 0,
+        isInternship: true,
+      },
+    ].concat(students);
+
+    setStudents(newStudents);
+    setCurrentRow({
+      Untitled: {
+        fullName: { value: "" },
+        email: { value: "" },
+      },
+    });
+    focusInput();
+  };
+  const onClose = () =>{
+      let newStudents = [...students];
+        newStudents.shift();
+      setStudents([...newStudents]);
+  }
+  const onSave = () =>{
+    let newStudents = [...students];
+    
+    newStudents[0] =  {
+        id: 'Untiled',
+        fullName: currentRow.Untitled.fullName.value,
+    }
+    setStudents([...newStudents]);
+    console.log(newStudents[0]);
+  }
+  const focusInput = () => {
+    setTimeout(() => {
+      if (
+        document.querySelectorAll(".MuiDataGrid-cell .MuiInputBase-input")
+          .length
+      ) {
+        document
+          .querySelectorAll(".MuiDataGrid-cell .MuiInputBase-input")[0]
+          .focus();
+      }
+    }, 1);
+  };
+
+  return (
+    <div className={styles["student-management"]}>
+      <Header>
+        <div>
+          <h5>Student Management</h5>
         </div>
-    );
+      </Header>
+      <Body>
+        <div className={styles["filter-table"]}>
+          {/* <RowEditControlGrid></RowEditControlGrid> */}
+          <FilterTable
+            columns={SMTypeModel(()=>{},()=>{},()=>{},()=>{})}
+            rows={students}
+            onChange={(model) => setCurrentRow(model)}
+            currentRow={currentRow}
+            onAdd={add}
+            onClose ={onClose}
+            onSave = {onSave}
+            // onSave
+            // onClose
+          />
+        </div>
+      </Body>
+    </div>
+  );
 }
 
 export default StudentManagement;
--- client/src/ui/Table/FilterTable/FilterTable.component.jsx
+++ client/src/ui/Table/FilterTable/FilterTable.component.jsx
@@ -1,8 +1,21 @@
 import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import styles from "./FilterTable.module.scss";
+import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import Button from "@mui/material/Button";
 
-export default function FiterTable({rows, columns}) {
+export default function FiterTable({
+  rows,
+  columns,
+  onChange,
+  currentRow,
+  onAdd,
+  onSave,
+  onClose,
+}) {
+  const [isAdd, setIsAdd] = React.useState(true);
+
   return (
     <div style={{ height: 400, width: "100%" }}>
       <DataGrid
@@ -12,6 +25,46 @@ export default function FiterTable({rows, columns}) {
         rowsPerPageOptions={[5]}
         checkboxSelection
         disableSelectionOnClick
+        onEditRowsModelChange={onChange}
+        components={{
+          Toolbar: () => {
+            return (
+              <div className="py-2">
+                {isAdd ? (
+                  <Button
+                    className="d-flex align-items-center"
+                    onClick={() => {
+                      onAdd();
+                      setIsAdd(false);
+                    }}
+                  >
+                    <div className="d-flex align-items-center">
+                      <AddOutlinedIcon />{" "}
+                      <span className="ml-2 pt-1">Add Record</span>
+                    </div>
+                  </Button>
+                ) : (
+                  <div>
+                    <Button onClick={() => {onSave(); setIsAdd(true)}}>
+                      <div className="d-flex align-items-center">
+                        <SaveOutlinedIcon />{" "}
+                        <span className="ml-2 pt-1">Save Record</span>
+                      </div>
+                    </Button>
+                    <Button onClick={() => {onClose(); setIsAdd(true)}}>
+                      <div className="d-flex align-items-center">
+                        <CloseOutlinedIcon />{" "}
+                        <span className="ml-2 pt-1">Close Record</span>
+                      </div>
+                    </Button>
+                  </div>
+                )}
+              </div>
+            );
+          },
+        }}
+        editRowsModel={currentRow}
+        editMode="row"
       />
     </div>
   );
