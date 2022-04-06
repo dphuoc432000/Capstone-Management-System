dae7d003435ae0e7db27a96b11b43d2a362dd641
ngthanhdat521 <84183823+ngthanhdat521@users.noreply.github.com>
2022-04-06 15:31:01 +0700

    update
    update sample document api

---
+++ server/controller/SampleDocumentController.js
@@ -0,0 +1,53 @@
+const sampleDocument = require("../service/SampleDocumentService");
+
+//api: /api/sampleDocument/
+class SampleDocumentController {
+    //Thêm folder
+    //POST: /sampleDocument/add
+    addFolder = (req, res, next) => {
+        let { pathName, folderName } = req.body;
+
+        if (folderName) {
+            let message = sampleDocument.addFolder(pathName, folderName);
+            res.status(200).json({ message });
+        } else
+            res.status(404).json({ message: "Folder or pathname is empty !" });
+    };
+
+    //Thêm file
+    //POST: /sampleDocument/addFiles
+    addFiles = async (req, res, next) => {
+        res.status(200).json({ message: "Successfully" });
+    };
+
+    //Lấy file
+    //GET: /sampleDocument/get
+    get = async (req, res, next) => {
+        let files = await sampleDocument.getFiles();
+        res.status(200).json(files);
+    };
+
+    //Download file
+    //POST: /sampleDocument/download
+    download = (req, res, next) => {
+        let { pathName } = req.query;
+
+        if (pathName) {
+            let fullPath = sampleDocument.downloadFileFromPath(pathName);
+            res.status(200).download(fullPath);
+        } else res.status(404).json({ message: "Path Name is empty !" });
+    };
+
+    //Delete file
+    //POST: /sampleDocument/delete
+    remove = (req, res, next) => {
+        let { pathName, name } = req.body;
+        console.log(req.body);
+        console.log("start");
+
+        sampleDocument.remove(pathName, name);
+        res.status(200).json({ message: "Successfully delete item !" });
+    };
+}
+
+module.exports = new SampleDocumentController();
--- server/index.js
+++ server/index.js
@@ -2,27 +2,26 @@ const initLibraries = require("./config/libraries");
 const Postgres = require("./db/postgresql/PostgreSQL");
 const server = initLibraries();
 const PORT = process.env.PORT || 5000;
-const router = require('./router/index');
-const db = require('./db/index');
-const path = require('path');
+const router = require("./router/index");
+const db = require("./db/index");
+const path = require("path");
 const { deleteFile } = require("./helper/handleFile");
 
 db.connect();
 router(server);
-server.get('/',(req,res)=>{
-  res.sendFile((path.join(__dirname,'/index.html')))
-})
+server.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
 
 //download
-server.get('/download', function(req, res){
-  const file = `${__dirname}/upload/3142022454411912-C1SE.31-ProductBacklogAndUserStory-v1.5.xlsx`;
-  res.download(file); // Set disposition and send it.
+server.get("/download", function (req, res) {
+    const file = `${__dirname}/upload/3142022454411912-C1SE.31-ProductBacklogAndUserStory-v1.5.xlsx`;
+    res.download(file); // Set disposition and send it.
 });
 
 //
 // console.log(deleteFile('\\upload\\roles.csv'));
 
-
 server.listen(PORT, () => {
-  console.log("Server run at " + PORT);
+    console.log("Server run at " + PORT);
 });
