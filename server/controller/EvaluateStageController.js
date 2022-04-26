c1182e38cc3224d0f3fc3aba5fdff27a00a57d4e
dphuoc432000 <dphuoc432000@gmail.com>
2022-04-27 00:02:22 +0700

    done evaluate stage

--- server/controller/EvaluateStageController.js
+++ server/controller/EvaluateStageController.js
@@ -9,7 +9,7 @@ class EvaluateStageController {
                     return res.status(200).send("Đánh giá thành công");
                 return res.status(400).send(`Lỗi thêm evaludate:
                     - Có một hoặc nhiều stage không thuộc project của student.
-                    - Có môt hoặc nhiều student đã được đánh giá trong stage này.`);//Lỗi thêm hoặc stageId khong thuộc project của student
+                    - Có môt hoặc nhiều student đã được đánh giá trong stage này.`);
             })
             .catch(err => res.status(500).send(err.message));
     }
@@ -20,6 +20,33 @@ class EvaluateStageController {
                     return res.status(400).send(`Không tìm thấy stage`);
                 else if(!data) 
                     return res.status(400).send(`Stage chưa được đánh giá`);
+                else 
+                    return res.status(200).json(data);
+            })
+            .catch(err => res.status(500).send(err.message));
+    }
+
+    updateEvaluates = async(req, res, next) =>{
+        await EvaluateStageService.updateEvaluates(req.body)
+            .then(data =>{
+                if(data === null){
+                    return res.status(400).send(`Evaluates rỗng`);
+                }
+                else if(!data) 
+                    return res.status(400).send(`Lỗi thêm evaludate:
+                    - Có một hoặc nhiều stage không thuộc project của student.
+                    - Có môt hoặc nhiều student không được tìm thấy đánh giá trong stage này.`);
+                else 
+                    return res.status(200).json(data);
+            })
+            .catch(err => res.status(500).send(err.message));
+    }
+
+    deleteAllEvaluatesOfStage = async(req, res, next) =>{
+        await EvaluateStageService.deleteAllEvaluatesOfStage(req.params.stageId)
+            .then(data =>{
+                if(!data) 
+                    return res.status(400).send(`Không tìm thấy đánh giá của stage này`);
                 else 
                     return res.status(200).json(data);
             })
--- server/service/EvaluateStageService.js
+++ server/service/EvaluateStageService.js
@@ -19,7 +19,6 @@ class EvaluateStageService {
                     projectId: stageData.projectId,
                 }, raw: true
             })
-            console.log(projectData);
             return await Student.findOne({
                 where:{
                     stuId,
@@ -84,5 +83,62 @@ class EvaluateStageService {
         })
     }
 
+    //cập nhật mảng evaluate;
+    updateEvaluates = async(evaluates) =>{
+        if(evaluates.length > 0){
+            //quét qua mảng đánh giá xem có student nào không hợp lệ không
+            for (const eva of evaluates) {
+                console.log(await this.checkStudentInEvaluateStage(eva.stageId, eva.stuId))
+                if(!await this.checkStudentInStage(eva.stageId, eva.stuId) || !await this.checkStudentInEvaluateStage(eva.stageId, eva.stuId)){
+                    return false;
+                }
+            }
+            //Nếu hợp lệ hết
+            //update
+            let evaluatesUpdate = [];
+            for(const eva of evaluates){
+                console.log(eva)
+                await EvaluateStage.update(eva,{
+                    where:{
+                        stuId: eva.stuId,
+                        stageId: eva.stageId
+                    }
+                }).then(async count =>{
+                    console.log("count",count)
+                    if(count[0] > 0){
+                        const evaluateData = await EvaluateStage.findOne({
+                            where:{
+                                stuId: eva.stuId,
+                                stageId: eva.stageId,
+                            }, raw: true
+                        })
+                        evaluatesUpdate.push(evaluateData);
+                    }
+                })
+                console.log(evaluatesUpdate)
+            }
+            return  evaluatesUpdate;
+        }
+        return null;
+    }
+
+    //xoá tất cả đánh giá của thành viên trong Stage này
+    deleteAllEvaluatesOfStage = async (stageId) =>{
+        
+        return await EvaluateStage.findAll({
+            where:{
+                stageId
+            }, raw: true
+        }).then(async data =>{
+            if(data){
+                return await EvaluateStage.destroy({
+                    where:{
+                        stageId
+                    }
+                }).then(() => data)
+            }
+            return false;
+        })
+    }
 }
 module.exports = new EvaluateStageService();
