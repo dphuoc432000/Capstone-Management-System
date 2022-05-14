3da53298650b5af545825c5d5cc23a5312ff32ad
TienNguyen-08 <cnnguyenvantien@gmail.com>
2022-05-15 01:40:38 +0700

    update

--- server/service/DefenseService.js
+++ server/service/DefenseService.js
@@ -147,115 +147,25 @@ class DefenseService {
                             attributes: ['firstName', 'lastName']
                         });
                     });
-                    getAllDefenseByLecturerId = async (lecturerId) => {
-                        let dataCouncil = await database.CouncilMember.findAll({
-                            where: {
-                                lecturerId: lecturerId
-                            },
-                            raw: true,
-                        }).then(async datas => {
-                            return await Promise.all(datas.map(async data => {
-                                return await database.Council.findOne({
-                                    where: {
-                                        councilId: data.councilId
-                                    }
-                                });
-                            }));
-                        })
-                        return await Promise.all(dataCouncil.map(async council => {
-                            let detailMembers = await database.CouncilMember.findAll({
-                                where: {
-                                    councilId: council.councilId
-                                }
-                            }).then(async members => {
-                                return await Promise.all(members.map(async member => {
-                                    let workUnit = member.workUnit;
-                                    let memberInfo = await database.Lecturer.findOne({
-                                        where: {
-                                            lecturerId: member.lecturerId
-                                        },
-                                        raw: true
-                                    }).then(async info => {
-                                        return await database.User.findOne({
-                                            where: {
-                                                userId: info.userId
-                                            },
-                                            attributes: ['firstName', 'lastName']
-                                        });
-                                    });
-                                    let firstName = memberInfo.firstName;
-                                    let lastName = memberInfo.lastName;
-                                    let lecturerId = member.lecturerId;
-                                    let role = await database.Role.findOne({
-                                        where: {
-                                            roleId: member.roleId
-                                        },
-                                        raw: true,
-                                    });
-                                    let roleName = role.roleName;
-                                    let roleId = role.roleId;
-                                    let dataMember = {
-                                        lecturerId,
-                                        workUnit,
-                                        firstName,
-                                        lastName,
-                                        roleId,
-                                        roleName
-                                    }
-                
-                                    return dataMember;
-                                }));
-                            });
-                
-                            var group = await database.Group.findOne({
-                                where: {
-                                    councilId: council.councilId
-                                },
-                                raw: true,
-                                attributes: ['groupId']
-                            });
-                
-                            let students = [];
-                            let mentors = [];
-                            let student = await database.Student.findAll({
-                                where: {
-                                    groupId: group.groupId
-                                },
-                                order: [
-                                    ["gpa", "DESC"]
-                                ],
-                                raw: true
-                            });
-                            if (student) {
-                
-                                for (let i = 0; i < student.length; i++) {
-                                    students.push(await StudentService.getStudent(student[i].userId));
-                                }
-                            }
-                
-                            let mentor = await database.GroupLecturer.findAll({
-                                where: {
-                                    groupId: group.groupId
-                                },
-                                raw: true
-                            });
-                            if (mentor) {
-                                for (let i = 0; i < mentor.length; i++) {
-                                    const user = await database.Lecturer.findOne({
-                                        where: {
-                                            lecturerId: mentor[i].lecturerId
-                                        },
-                                    });
-                                    mentors.push(await LecturerService.getLecturerByUserId(user.userId));
-                                }
-                            }
-                            return {
-                                council,
-                                students,
-                                mentors,
-                                detailMembers
-                            }
-                        }))
+
+                    let firstName = memberInfo.firstName;
+                    let lastName = memberInfo.lastName;
+                    let lecturerId = member.lecturerId;
+                    let role = await database.Role.findOne({
+                        where: {
+                            roleId: member.roleId
+                        },
+                        raw: true,
+                    });
+                    let roleName = role.roleName;
+                    let roleId = role.roleId;
+                    let dataMember = {
+                        lecturerId,
+                        workUnit,
+                        firstName,
+                        lastName,
+                        roleId,
+                        roleName
                     }
                     return dataMember;
                 }));
